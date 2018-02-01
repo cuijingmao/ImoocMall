7df1ec98dcdf90f063df202d2c377e8436a56077
happyC <cuijingmao12@163.com>
2018-02-01 13:39:14 +0800

    2-5vue基础语法：emit实现父子组件通信(counter.vue)

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+      <button v-on:click="increment">+</button>
+      <button v-on:click="decrement">-</button>
+      <p><span>{{num}}</span></p>
+    </div>
+</template>
+
+<script>
+    export default {
+      props:["num"],
+      data(){
+        return {
+          num:0
+        }
+      },
+      methods:{
+        increment(){
+          this.$emit("incre");
+
+        },
+        decrement(){
+          this.$emit("decre");
+        }
+      }
+    }
+</script>
