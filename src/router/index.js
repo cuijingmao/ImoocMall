f0a4b653b2ad74618ce814656f8fa07efa52c898
willC <cuijingmao12@163.com>
2018-02-01 21:32:04 +0800

    3-3嵌套路由(Iamge.vue,Title.vue)

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from './../views/GoodsList.vue'
-
+import Title from '@/views/Title'
+import Image from '@/views/Image'
 Vue.use(Router)
 
 export default new Router({
   mode:'hash',
   routes: [
     {
-      path: '/goods/:goodsId/user/:name',
+      path: '/goods',
       name: 'GoodsList',
-      component: GoodsList
+      component: GoodsList,
+      children:[
+        {
+          path:'title',
+          name:'title',
+          component:Title
+        },
+        {
+          path:'img',
+          name:'img',
+          component:Image
+        }
+      ]
+
+
     }
   ]
 })
