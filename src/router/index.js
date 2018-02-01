e104087ad02afa47a8ffd90940f7da848cffabf8
willC <cuijingmao12@163.com>
2018-02-01 20:51:43 +0800

    3-2动态路由设置(GoodsList.vue)）

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from './../views/GoodsList.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'hash',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods/:goodsId/user/:name',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
